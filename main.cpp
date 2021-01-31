df0cff92ec635eb3438f23d8704ac64d5b4a512e
attilathedud <attilathedud@gmail.com>
2021-01-31 00:05:28 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ A triggerbot for Assault Cube that fires the player's weapon whenever the crossh
 This works by hooking the game's feature that displays nametags when you hover a player. Whenever a player is hovered over, our codecave
 will send a mouse down event to the game. Otherwise, it will send a mouse up event to stop firing.
 
-This must be injected into the Urban Terror process to work. One way to do this is to use a DLL injector. 
+This must be injected into the Assault Cube process to work. One way to do this is to use a DLL injector. 
 Another way is to enable AppInit_DLLs in the registry.
 
 The offsets and method to discover them are discussed in the article at: https://gamehacking.academy/lesson/26
